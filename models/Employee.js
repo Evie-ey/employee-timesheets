89a7308ab640b12cdbd21bd13280d70496cd1a72
Evelyne Namwoyo <evelyne@laboremus.no>
2020-08-12 15:02:16 +0200

    Added employee model

---
+++ models/Employee.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+const schema = mongoose.Schema;
+
+const EmployeeSchema = new Schema({
+  username: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = Employee = mongoose.model('Employee', EmployeeSchema)
