da7154c8498059f4ff0c5729adb6f4d92f156f70
Evelyne Namwoyo <evelyne@laboremus.no>
2020-08-13 15:14:10 +0200

    Created Project model

---
+++ models/Project.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+const schema = mongoose.Schema;
+
+const ProjectSchema = new schema({
+  company: {
+    type: String,
+    required: true
+  },
+  name: {
+    type: String,
+    required: true
+  },
+  code: {
+    type: String,
+    required: true
+  },
+  projectItem: [{type: Schema.Types.ObjectId, ref: 'ProjectItem'}],
+
+  date: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = Project = mongoose.model('Project', ProjectSchema)
